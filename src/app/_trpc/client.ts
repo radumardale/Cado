b861836351ca7336f4f8e1c5cb4ca6a6f7c88d89
TudorModVis <tudorache.on@gmail.com>
2025-06-02 15:12:11 +0300

    invalidate cashe test

--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -2,4 +2,23 @@ import { createTRPCReact } from "@trpc/react-query";
 
 import { type AppRouter } from "@/server";
 
-export const trpc = createTRPCReact<AppRouter>();
+export const trpc = createTRPCReact<AppRouter>({
+    overrides: {
+      useMutation: {
+        /**
+         * This function is called whenever a `.useMutation` succeeds
+         **/
+        async onSuccess(opts) {
+          /**
+           * @note that order here matters:
+           * The order here allows route changes in `onSuccess` without
+           * having a flash of content change whilst redirecting.
+           **/
+          // Calls the `onSuccess` defined in the `useQuery()`-options:
+          await opts.originalFn();
+          // Invalidate all queries in the react-query cache:
+          await opts.queryClient.invalidateQueries();
+        },
+      },
+    },
+  });
