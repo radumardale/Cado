52a8b1900aaf6fe46d18fd9afb9142a4caa825b7
VadimOMEGA <vadimivanov.0320@gmail.com>
2025-06-17 23:02:23 +0300

    minor fix

--- src/components/catalog/productsGrid/Controls.tsx
+++ src/components/catalog/productsGrid/Controls.tsx
@@ -18,7 +18,7 @@ export default function Controls({gridLayout, setGridLayout, setSortBy, setSideb
   const t = useTranslations("CatalogPage.ProductsSection.sort_options")
 
   return (
-    <div className='col-span-full flex gap-1 lg:gap-2 justify-end sticky lg:relative top-14 lg:top-auto py-2 lg:py-0 left-0 z-40 bg-white lg:z-auto lg:bg-transparent items-center h-12'>
+    <div className='col-span-full flex gap-1 lg:gap-2 justify-end sticky lg:relative top-14 lg:top-auto py-2 lg:py-0 left-0 z-40 bg-white lg:z-auto lg:bg-transparent items-center'>
         {searchText && <h2 className='font-manrope text-2xl leading-7 uppercase font-semibold mr-auto'>{countProducts} {countProducts === 1 ? "REZULTAT" : "REZULTATE"} CU “{searchText.split("+").join(" ")}”</h2>}
         <div className='flex gap-1 lg:gap-2 w-full lg:w-auto'>
             <button onClick={() => {setSidebarOpen(true)}} className={`lg:hidden mr-auto size-12 rounded-full border border-black flex justify-center items-center transition duration-300 hover:bg-black cursor-pointer ${isSidebarOpen ? "bg-black text-white" : ""} hover:text-white`}>
