da513f0011e32c3174b6d69b74a6ac2dc9baca50
VadimOMEGA <vadimivanov.0320@gmail.com>
2025-06-17 13:11:18 +0300

    Minor fixes

--- src/components/product/AdditionalInfo.tsx
+++ src/components/product/AdditionalInfo.tsx
@@ -25,6 +25,8 @@ export default function AdditionalInfo({product, locale}: AdditionalInfoInterfac
                     className={styles.productDescription + " text-sm lg:text-base leading-4 lg:leading-5"}
                     dangerouslySetInnerHTML={{ __html: product.set_description[locale] }}
                 ></div>
+
+                <p className='mb-6 font-bold'>*{prod_t('includes_info')} <a href="mailto:order@cado.md" className='underline'>order@cado.md</a></p>
             </Accordion>
         }
         <Accordion productAccordion open={activeIndex == 1} setActiveIndex={() => {setActiveIndex(activeIndex === 1 ? -1 : 1)}} title={prod_t('description')}>
