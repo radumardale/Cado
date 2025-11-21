db02e2fb159587481bb368f5d2f6a2ced0240bf3
Radu Mardale <radu.mardale@gmail.com>
2025-11-21 19:58:56 +0200

    One more nitpick

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -30,7 +30,7 @@ interface HeaderProps {
 export default function Header({ category, breadcrumbs = false, productInfo }: HeaderProps) {
   const locale = useLocale() as LocaleCode;
 
-  const getLogoSrc = (locale: string) => {
+  const getLogoSrc = (locale: LocaleCode) => {
     switch (locale) {
       case 'en':
         return '/logo/CADO-en.svg';
