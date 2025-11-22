52dfcf8bfc92c2fe94afa591c0d18c9385134cf5
Radu Mardale <radu.mardale@gmail.com>
2025-11-22 12:45:29 +0200

    fix(lint): remove unused AWS SDK imports from image tests

--- src/__tests__/procedures/image/imageProcedures.test.ts
+++ src/__tests__/procedures/image/imageProcedures.test.ts
@@ -44,7 +44,7 @@ import { Blog } from '@/models/blog/blog';
 import { HomeBanner } from '@/models/home_banner/HomeBanner';
 import connectMongo from '@/lib/connect-mongo';
 import { createMockDocument } from '@/__tests__/helpers/testUtils';
-import { S3Client, DeleteObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { deleteFromBucket } from '@/server/procedures/image/deleteObjects/deleteFromBucket';
 import { selectObjectToDelete } from '@/server/procedures/image/deleteObjects/selectObjectToDelete';
