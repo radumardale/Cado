af1afb2d26447ca7033d0a5c7140d528f26064fa
TudorModVis <tudorache.on@gmail.com>
2025-11-24 19:31:51 +0200

    fixed paynet products array bug

--- src/server/procedures/order/addOrder.ts
+++ src/server/procedures/order/addOrder.ts
@@ -154,6 +154,7 @@ export const addOrderProcedure = protectedProcedure
       }
 
       if (input.payment_method === OrderPaymentMethod.Paynet) {
+
         const requestBody = {
           Invoice: order.invoice_id,
           MerchantCode: process.env.API_MERCHANT_CODE,
@@ -182,22 +183,14 @@ export const addOrderProcedure = protectedProcedure
               Name: "CADO Order",
               Description: `Order #${order.custom_id}`,
               Amount: Math.round(input.total_cost * 100),
-              Products: input.products.map((product: any, index: number) => ({
-                GroupName: "Produse",
-                GroupId: 1,
-                LineNo: index + 1,
-                Code: product.product.custom_id,
-                Barcode: index + 1001,
-                Name: product.product.title.ro,
-                Description: product.product.title.ro,
-                UnitPrice: Math.round(product.product.sale && product.product.sale.active ? product.product.sale.sale_price * 100 : product.product.price * 100),
-                UnitProduct: product.quantity,
-                Amount: Math.round((product.product.sale && product.product.sale.active ? product.product.sale.sale_price * 100 : product.product.price * 100) * product.quantity)
-              }))
+              Products: []
             }
           ],
           MoneyType: null
         };
+
+        console.log("Services: \n", requestBody.Services[0]);
+        console.log("Products: \n", requestBody.Services[0].Products);
   
         const response = await APIClient.makeAuthenticatedRequest(
           `${process.env.API_BASE_URL}/api/Payments/Send`,
@@ -207,6 +200,8 @@ export const addOrderProcedure = protectedProcedure
           }
         );
         const data = await response.json();
+
+        console.log("Data: \n", data);
 
         return {
           success: true,
@@ -245,7 +240,7 @@ export const addOrderProcedure = protectedProcedure
           subject: subjectLang.get("ro"),
           html: emailHtml
       }
-
+      
       await transporter.sendMail(emailData);
 
       return {
